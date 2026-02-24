2501b897ada883cd552286369d2cd86388d6186f
Muhammad Bonn <118687900+muhammadbonn@users.noreply.github.com>
2026-02-24 09:32:00 +0200

    Fix table creation and column naming in Olist Bronze DDL
    Updated SQL script to create Olist Bronze tables with corrections in column names and added missing tables. Removed unnecessary schema alterations and loading instructions.

---
+++ scripts/bronze/201_olist_bronze_ddl.sql
@@ -0,0 +1,138 @@
+-----------------------
+-- Create Olist Bronze Tables
+-----------------------
+USE ecommerceDWH;
+Go
+
+--- Customers Table
+IF OBJECT_ID('bronze.olist_customers', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_customers;
+GO
+
+CREATE TABLE bronze.olist_customers (
+  customer_id TEXT,
+  customer_unique_id TEXT,
+  customer_zip_code_prefix INTEGER,
+  customer_city TEXT,
+  customer_state TEXT
+);
+GO
+
+--- Geolocation Table
+IF OBJECT_ID('bronze.olist_geolocation', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_geolocation;
+GO
+
+CREATE TABLE bronze.olist_geolocation (
+  geolocation_zip_code_prefix INTEGER,
+  geolocation_lat NUMERIC,
+  geolocation_lng NUMERIC,
+  geolocation_city TEXT,
+  geolocation_state TEXT
+);
+GO
+  
+--- Sellers Table
+IF OBJECT_ID('bronze.olist_sellers', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_sellers;
+GO
+  
+CREATE TABLE bronze.olist_sellers.sellers (
+  seller_id TEXT,
+  seller_zip_code_prefix INTEGER,
+  seller_city TEXT,
+  seller_state TEXT
+);
+GO
+
+--- Products Table
+IF OBJECT_ID('bronze.olist_products', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_products;
+GO
+
+CREATE TABLE bronze.olist_products (
+  product_id TEXT,
+  product_category_name TEXT,
+  product_name_length INTEGER,
+  product_description_length INTEGER,
+  product_photos_qty INTEGER,
+  product_weight_g NUMERIC,
+  product_length_cm NUMERIC,
+  product_height_cm NUMERIC,
+  product_width_cm NUMERIC
+);
+GO
+
+--- Orders Table
+IF OBJECT_ID('bronze.olist_orders', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_orders;
+GO
+  
+CREATE TABLE bronze.olist_orders (
+  order_id TEXT,
+  customer_id TEXT,
+  order_status TEXT,
+  order_purchase_timestamp TIMESTAMP,
+  order_approved_at TIMESTAMP,
+  order_delivered_carrier_date TIMESTAMP,
+  order_delivered_customer_date TIMESTAMP,
+  order_estimated_delivery_date TIMESTAMP
+);
+GO
+
+--- Orders Items Table
+IF OBJECT_ID('bronze.olist_order_items', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_order_items;
+GO
+  
+CREATE TABLE order_items (
+  order_id TEXT,
+  order_item_id INTEGER,
+  product_id TEXT,
+  seller_id TEXT,
+  shipping_limit_date TIMESTAMP,
+  price NUMERIC(12,2),
+  freight_value NUMERIC(12,2)
+);
+GO
+
+--- Orders Payments Table
+IF OBJECT_ID('bronze.order_payments', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_order_payments;
+GO
+  
+CREATE TABLE order_payments (
+  order_id TEXT,
+  payment_sequential INTEGER,
+  payment_type TEXT,
+  payment_installments INTEGER,
+  payment_value NUMERIC(12,2)
+);
+GO
+
+--- Orders Reviews Table
+IF OBJECT_ID('bronze.order_reviews', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_order_reviews;
+GO
+
+CREATE TABLE bronze.olist_order_reviews (
+  review_id TEXT,
+  order_id TEXT,
+  review_score INTEGER,
+  review_comment_title TEXT,
+  review_comment_message TEXT,
+  review_creation_date TIMESTAMP,
+  review_answer_timestamp TIMESTAMP
+);
+GO
+
+--- Product Category Name Translation Table
+IF OBJECT_ID('bronze.olist_product_category_name_translation', 'U') IS NOT NULL
+    DROP TABLE bronze.olist_product_category_name_translation;
+GO
+
+CREATE TABLE bronze.olist_product_category_name_translation (
+  product_category_name TEXT,
+  product_category_name_english TEXT
+);
+GO
