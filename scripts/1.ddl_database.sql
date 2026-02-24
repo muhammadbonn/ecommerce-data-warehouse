00eaf7130d6d4cd11d78f16bcb9abf5f13612940
Muhammad Bonn <118687900+muhammadbonn@users.noreply.github.com>
2026-02-24 07:56:41 +0200

    Rename database from maventoy to ecommerceDWH

---
+++ scripts/1.ddl_database.sql
@@ -0,0 +1,21 @@
+-----------------------
+-- Create Database
+-----------------------
+USE master;
+CREATE DATABASE ecommerceDWH;
+Go
+
+USE ecommerceDWH;
+Go
+
+-----------------------
+-- Create Schemas
+-----------------------
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
